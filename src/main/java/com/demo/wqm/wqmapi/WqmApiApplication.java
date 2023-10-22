13ee6766fbed0f80fbd12de7a6b0e6fb97a07c32
Mr.H <mike.hastie@gmail.com>
2023-10-22 18:19:35 -0400

    first commit

---
+++ src/main/java/com/demo/wqm/wqmapi/WqmApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.wqm.wqmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WqmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WqmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/wqm/wqmapi/WqmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.wqm.wqmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WqmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
